96545a69d338f806ad1e80cebf8f7ad73e00504a
enriquedell <enriquedell@gmail.com>
2026-09-17 04:51:38 +0330

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,16 +31,28 @@ android {
 }
 
 dependencies {
+
+    val composeBom =
+        platform("androidx.compose:compose-bom:2025.01.00")
+
+    implementation(composeBom)
+
     implementation("androidx.core:core-ktx:1.15.0")
+
     implementation("androidx.activity:activity-compose:1.10.1")
+
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
 
     implementation("androidx.compose.ui:ui")
+
     implementation("androidx.compose.ui:ui-tooling-preview")
+
     implementation("androidx.compose.material3:material3")
+
     implementation("androidx.compose.material:material-icons-extended")
 
     implementation("androidx.navigation:navigation-compose:2.8.5")
+
     implementation("androidx.core:core-splashscreen:1.0.1")
 
     debugImplementation("androidx.compose.ui:ui-tooling")
